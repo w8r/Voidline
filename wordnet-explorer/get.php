8d4606c64208d289f11b09a934a8ca752392700e
Emilian Manolache <e.manolache@dataclaritycorp.com>
2017-10-19 01:27:33 +0300

    Added ForceLink algorithm and option to retrieve all data (too large to handle currently)

--- wordnet-explorer/get.php
+++ wordnet-explorer/get.php
@@ -7,38 +7,79 @@ $data = Array();
 class getter {
 	private $results = null;
 	private $db = null;
-	private $startID = null;
 	private $subsequent = null;
-	
+	private $all = false;
+    private $children = null;
+
 	function __construct() {
 		$this->db = new database();
-		$this->startID = $_GET["id"];
 		$this->subsequent = $_GET["subsequent"] == "true" ? true : false;
-	}
-	
-	function getEdges() {
-		$query = "SELECT synset_id_1 as source, synset_id_2 as target FROM wn_hyponym WHERE synset_id_1 = " . $this->startID;		
-		$this->db->query($query);
-		$results = $this->db->loadResults();
-		
+		$this->all = $_GET["all"] == "true" ? true : false;
 		$this->results = new \stdClass;
 		$this->results->edges = array();
-		
-		$children = array($this->startID);
-		for ($i = 0; $i < count($results); $i++) {
-			$this->results->edges[$i] = $results[$i];
-			$this->results->edges[$i]["id"] = uniqid();
-			$children[($i + 1)] = $results[$i]["target"];
-		}
-		
-		$this->results->nodes = $this->getNodes($children);
-		return $this->results;
+		$this->results->nodes = array();
+		$this->children = array();
 	}
 
-	function getNodes($edges) {
+	function startGet($node) {
+	    if (!$this->all) {
+            $this->getEdges($node);
+            return $this->results;
+	    }
+	    else {
+	        $this->getAllEdges($node);
+	        return $this->results;
+	    }
+	}
+
+    function getAllEdges($node) {
+		$query = "SELECT synset_id_1 as source, synset_id_2 as target FROM wn_hyponym";
+		$this->db->query($query);
+		$results = $this->db->loadResults();
+
+		for ($i = 0; $i < count($results); $i++) {
+		    array_push($this->results->edges, $results[$i]);
+		    $lastItem = count($this->results->edges) - 1;
+			$this->results->edges[$lastItem]["id"] = uniqid();
+		}
+
+		$this->getAllNodes();
+	}
+
+	function getAllNodes() {
+        $query = "SELECT a.synset_id as id, proper(replace(a.word, '_', ' ')) as text, b.gloss as description FROM wn_synset a INNER JOIN wn_gloss b ON a.synset_id = b.synset_id " .
+                 "GROUP BY a.synset_id";
+        $this->db->query($query);
+        $results = $this->db->loadResults();
+
+        for ($i = 0; $i < count($results); $i++) {
+            $results[$i]["data"] = new \stdClass;
+            $results[$i]["data"]->description = $results[$i]["description"];
+            unset($results[$i]["description"]);
+            array_push($this->results->nodes, $results[$i]);
+        }
+    }
+
+	function getEdges($node) {
+		$query = "SELECT synset_id_1 as source, synset_id_2 as target FROM wn_hyponym WHERE synset_id_1 = " . $node;
+		$this->db->query($query);
+		$results = $this->db->loadResults();
+
+		$children = array($node);
+		for ($i = 0; $i < count($results); $i++) {
+		    array_push($this->results->edges, $results[$i]);
+		    $lastItem = count($this->results->edges) - 1;
+			$this->results->edges[$lastItem]["id"] = uniqid();
+			$children[($i + 1)] = $results[$i]["target"];
+		}
+
+        $this->getNodes($children, $node);
+	}
+
+	function getNodes($edges, $node) {
 		$query = "SELECT a.synset_id as id, proper(replace(a.word, '_', ' ')) as text, b.gloss as description FROM wn_synset a INNER JOIN wn_gloss b ON a.synset_id = b.synset_id " . 
 				 "WHERE a.synset_id IN (" . join($edges, ",") . ") " .
-				 ($this->subsequent ? ("AND a.synset_id != " . $this->startID . " ") : "") .
+				 ($this->subsequent ? ("AND a.synset_id != " . $node . " ") : "") .
 				 "GROUP BY a.synset_id";
 		$this->db->query($query);
 		$results = $this->db->loadResults();
@@ -47,11 +88,11 @@ class getter {
 		    $results[$i]["data"] = new \stdClass;
 		    $results[$i]["data"]->description = $results[$i]["description"];
 		    unset($results[$i]["description"]);
+		    array_push($this->results->nodes, $results[$i]);
         }
-		return $results;
 	}
 }
 $getter = new getter();
-$results = $getter->getEdges();
+$results = $getter->startGet($_GET["id"]);
 die(json_encode($results));
 ?>
--- wordnet-explorer/voidline.js
+++ wordnet-explorer/voidline.js
@@ -1,12 +1,29 @@
-var startID = 100001740;
 var ogma = null;
+
+var settings = {
+    loadAll: false,
+    startNode: 100001740,
+    algorithm: 'forcelink',
+    all: false
+};
+
+function updateGraph() {
+    switch(settings.algorithm) {
+        case "hierarchical":
+            performDAG();
+            break;
+        case "forcelink":
+            performFL();
+            break;
+    }
+}
 
 function loadData(nodeID, subsequent) {
     if (typeof (subsequent) == "undefined") subsequent = false;
-    $.getJSON(("/voidline/wordnet-explorer/get.php?id=" + nodeID + "&subsequent=" + subsequent), function(data) {
+    $.getJSON(("/voidline/wordnet-explorer/get.php?id=" + nodeID + "&subsequent=" + subsequent + "&all=" + settings.all), function(data) {
         ogma.graph.addNodes(data.nodes);
         ogma.graph.addEdges(data.edges);
-        performDAG();
+        updateGraph();
     });
 }
 
@@ -18,13 +35,24 @@ function performDAG() {
         duration: 300,  // Duration of the animation
         nodesep: 30,     // Number of pixels that separate nodes horizontally in the layout.
         ranksep: 40     // Number of pixels between each rank in the layout.
-    }, {
-        //onEnd:endLayout
     });
 }
 
-function endLayout() {
-    console.log('done');
+function performFL() {
+    ogma.locate.center();
+
+    ogma.layouts.start('forceLink', {
+        // layout parameters
+        autoStop: true,
+        iterationsPerRender: 1,
+        startingIterations: 1,
+        slowDown: 2
+    }, {
+        duration: 0
+    });
+}
+
+function centerGraph() {
     ogma.locate.center({
         easing: 'linear',
         duration: 300
@@ -52,5 +80,5 @@ $(document).ready(function() {
         return '<div>' + node.data.description + '</div>';
     });
 
-    loadData(startID, false);
+    loadData(settings.startNode, false);
 });
